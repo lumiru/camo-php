9693f43a1690b47c384b25120d937f2997cdf883
lumiru <lumiru@live.fr>
2016-10-23 02:02:34 +0200

    Add comments

--- camo.php
+++ camo.php
@@ -1,7 +1,13 @@
 <?php
 
+// Shared key
 define('SERVER_KEY', '24FEEDFACEDEADBEEFCAFE');
 
+/**
+ * Get requested image URL
+ * @param  string $key Used shared key
+ * @return string      Image URL
+ */
 function get_camo_requested_url($key)
 {
 	$requestedUrl = null;
@@ -30,6 +36,11 @@ function get_camo_requested_url($key)
 	return $requestedUrl;
 }
 
+/**
+ * Get headers received from client
+ * @param  array $names Header keys to retreive
+ * @return array        Headers
+ */
 function get_request_headers(array $names)
 {
 	$headers = array();
@@ -46,6 +57,12 @@ function get_request_headers(array $names)
 	return $headers;
 }
 
+/**
+ * Open HTTP stream
+ * @param  string     $url     Stream URI
+ * @param  array|null $headers Headers to send
+ * @return Resource            HTTP Stream
+ */
 function open_http_stream($url, array $headers = null)
 {
 	$stream = null;
@@ -63,6 +80,11 @@ function open_http_stream($url, array $headers = null)
 	return $stream;
 }
 
+/**
+ * Transform key-value associated array to header formatted rows
+ * @param  array $headers Array by 'Key' => 'value'
+ * @return array          Array by 'Key: value'
+ */
 function headers_key_to_value(array $headers)
 {
 	return array_map(
@@ -71,6 +93,11 @@ function headers_key_to_value(array $headers)
 	);
 }
 
+/**
+ * Get HTTP headers returned by stream
+ * @param  Resource $stream HTTP stream
+ * @return array            HTTP headers
+ */
 function get_http_stream_headers($stream)
 {
 	$headers = array();
@@ -92,6 +119,11 @@ function get_http_stream_headers($stream)
 	return $headers;
 }
 
+/**
+ * Send headers to client
+ * @param  array      $headers HTTP headers
+ * @param  array|null $filters Headers keys to send (if available in $headers)
+ */
 function pass_headers(array $headers, array $filters = null)
 {
 	if (!is_null($filters)) {
@@ -110,14 +142,14 @@ function pass_headers(array $headers, array $filters = null)
 $url = get_camo_requested_url(SERVER_KEY);
 
 if(!empty($url)) {
+	// Custom headers
 	$sentHeaders = array(
 		'Accept' => 'image/*',
 		'Connection' => 'close',
 		'X-Frame-Options' => 'deny',
 		'X-XSS-Protection' => '1; mode=block',
 		'X-Content-Type-Options' => 'nosniff',
-		'Content-Security-Policy' => 'default-src \'none\'; img-src data:; style-src \'unsafe-inline\'',
-		'Accept-Encoding' => $_SERVER['HTTP_ACCEPT_ENCODING']
+		'Content-Security-Policy' => 'default-src \'none\'; img-src data:; style-src \'unsafe-inline\''
 	);
 	$receivedHeaders = get_request_headers(array(
 		'Accept-Encoding', 'If-Modified-Since', 'If-None-Match'
@@ -125,9 +157,11 @@ if(!empty($url)) {
 
 	$sentHeaders = array_merge($sentHeaders, $receivedHeaders);
 
+	// Get HTTP request
 	$stream = open_http_stream($url, $sentHeaders);
 	$headers = get_http_stream_headers($stream);
 
+	// If status is OK, returns the content and headers
 	if($headers['Status-Code'] === 200) {
 		if(strpos($headers['Content-Type'], 'image/') === 0) {
 			$headers['Cache-Control'] = 'public, max-age=31536000';
@@ -146,12 +180,14 @@ if(!empty($url)) {
 			readfile('403.html');
 		}
 	}
+	// Otherwise, only returns status header
 	else {
 		header($headers['Status']);
 	}
 
 	fclose($stream);
 }
+// If we cannot retreive the requested URL
 else {
 	header('HTTP/1.1 404 Not Found');
 	echo 'Error 404 - Not found';
